843238da49f73aae28d7391d83d77300132f5213
Balraj Gahlot <97168376+Balraj4347@users.noreply.github.com>
2022-06-08 18:38:38 +0530

    Create 24. Swap Nodes in Pairs.cpp
    https://leetcode.com/problems/swap-nodes-in-pairs/

---
+++ 24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,27 @@
+
+//49% faster
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head){
+        
+        if(!head || !head->next)
+            return head;
+        ListNode* ahead= head->next;
+        head->next = swapPairs(ahead->next);
+        ahead->next= head;
+        
+        return ahead;
+       
+        
+    }
+};
