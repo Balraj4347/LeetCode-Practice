87f15fedb0a378ab3408e687367b35fd059f2b38
Balraj Gahlot <97168376+Balraj4347@users.noreply.github.com>
2022-06-09 16:53:42 +0530

    Update 167. Two Sum II - Input Array Is Sorted.cpp

--- 167. Two Sum II - Input Array Is Sorted.cpp
+++ 167. Two Sum II - Input Array Is Sorted.cpp
@@ -3,29 +3,7 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int beg=0, end=numbers.size()-1;
         vector<int>result(2,0);
-        
-        while(beg<end){
-            int mid= beg +(end-beg)/2;
-            if(numbers[mid] < target)
-                beg=mid+1;
-            else if(numbers[mid]>target)
-                end= mid-1;
-            else 
-            {   break;
-            }
-         
-        }
-
-        if(target>=0){
-            beg=0;
-            end = end== numbers.size()-1 ? end: end+1;
-        }
-        else{
-          if(end!=numbers.size()-1)
-            while(numbers[++end]<0);
-                
-        }
-       
+    
         while(beg<end){
             int sum =numbers[beg]+numbers[end];
             if(sum < target)
