f48ebb70080cb341430f72e0717124423cacd9be
Balraj Gahlot <97168376+Balraj4347@users.noreply.github.com>
2022-06-30 15:36:22 +0530

    Create 462. Minimum Moves to Equal Array Elements II.cpp
    https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/

---
+++ 462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+    
+        int middle= nums[n/2];
+
+        int result=0;
+        for(int i:nums)
+            result += abs(i- middle);
+  
+        return result;
+    }
+};
